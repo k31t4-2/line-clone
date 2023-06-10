f92fabe228c1bb226959245a9dea2cdf9cdfe091
KEITA <ksekimotonz@gmail.com>
2023-06-11 08:39:41 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import SignIn from "./Components/SignIn";
+import Line from "./Components/Line";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 
 function App() {
+
+  // 自分がログインしているのかを管理するための変数
+  const user = useAuthState(auth);
+
   return (
     <div>
-      <SignIn/>
+
+      {/* ログイン状態　＝ true / ログアウト = false */}
+      {/* ログインしているなら、lineの画面に飛ぶ */}
+      {/* ログアウトなら、サインインの画面に飛ぶ */}
+
+      {user ? <Line /> : <SignIn />}
     </div>
   );
 }
---
+++ src/Components/Line.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@mui/material";
+import firebase from "firebase/compat/app";
+import { auth } from "../firebase"; 
+
+function Line() {
+  return (
+    <div>Line</div>
+  )
+}
+
+export default Line
